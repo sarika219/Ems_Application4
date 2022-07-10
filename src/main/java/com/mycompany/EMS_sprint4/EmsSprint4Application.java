88f79d572311941cb4caa4873105b94c1d6daacf
Abhisarika Tidke <sarikat999@gmail.com>
2022-07-10 16:16:46 +0530

    Changes

---
+++ src/main/java/com/mycompany/EMS_sprint4/EmsSprint4Application.java
@@ -0,0 +1,13 @@
+package com.mycompany.EMS_sprint4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmsSprint4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmsSprint4Application.class, args);
+	}
+
+}
